f3d8efe7dc37ebc72fc9be6338cd7e0f0e5115e0
Patrick Ohly <patrick.ohly@intel.com>
2015-11-25 17:38:45 +0100

    rmwork.py: run do_rm_work more aggressively
    Often when bitbake runs out of disk space under TravisCI, the
    "downloads" directory (and presumably also the temp directory)
    contains files from completed recipes which would have been removed if
    their do_rm_work tasks had been run.
    
    do_rm_work seems to run reliably for virtual native recipes directly after
    building the recipe, but not for target recipes. Example is below.
    
    The new scheduler tries to address this by making do_rm_work tasks the
    one with the highest priority and running them even when the maximum
    number of running tasks has been reached.
    
    In addition, debug output is always produced which helps debug priotities further.
    
    $ bitbake core-image-minimal
    [...]
    NOTE: recipe qemu-native-2.4.0.1-r1: task do_populate_sysroot: Started
    NOTE: recipe qemu-native-2.4.0.1-r1: task do_populate_sysroot: Succeeded
    NOTE: Running noexec task 1373 of 2269 (ID: 145, virtual:native:.../qemu_2.4.0.1.bb, do_build)
    NOTE: Running task 1374 of 2269 (ID: 146, virtual:native:.../qemu_2.4.0.1.bb, do_rm_work)
    NOTE: recipe qemu-native-2.4.0.1-r1: task do_rm_work: Started
    NOTE: recipe qemu-native-2.4.0.1-r1: task do_rm_work: Succeeded
    ...
    NOTE: recipe gmp-6.0.0-r0: task do_packagedata: Succeeded
    NOTE: Running task 1456 of 2269 (ID: 2168, .../gmp_6.0.0.bb, do_package_write_ipk)
    NOTE: recipe gmp-6.0.0-r0: task do_package_write_ipk: Started
    NOTE: recipe gmp-6.0.0-r0: task do_package_write_ipk: Succeeded
    
    [... nothing more for gmp; instead, several other tasks run ...]
    
    NOTE: Running task 1493 of 2269 (ID: 1801, .../libcheck_0.10.0.bb, do_fetch)
    ERROR: No new tasks can be executed since the disk space monitor action is "STOPTASKS"!
    WARNING: The free space of .../downloads (rootfs) is running low (0.487GB left)
    ERROR: No new tasks can be executed since the disk space monitor action is "STOPTASKS"!
    [...]
    $ (du -h -c -s $(ls -1 -d downloads/* downloads/git2/* | grep -v -e '.done$' -e '/git2$')) || true
    752K	downloads/check-0.10.0.tar.gz
    1.6M	downloads/git2/github.com.Samsung.cynara.git
    53M	downloads/git2/github.com.systemd.systemd.git
    973M	downloads/git2/git.yoctoproject.org.linux-yocto-4.1.git
    2.1M	downloads/git2/git.yoctoproject.org.prelink-cross.git
    22M	downloads/git2/git.yoctoproject.org.yocto-kernel-cache
    192K	downloads/git2/review.tizen.org.platform.core.appfw.tizen-platform-wrapper
    1.3M	downloads/git2/review.tizen.org.platform.upstream.smack
    272M	downloads/git2/sourceware.org.git.binutils-gdb.git
    0	downloads/git2/sourceware.org.git.binutils-gdb.git.lock
    2.3M	downloads/gmp-6.0.0a.tar.bz2
    1.3G	total
    
    Signed-off-by: Patrick Ohly <patrick.ohly@intel.com>

---
+++ scripts/rmwork.py
@@ -0,0 +1,76 @@
+# Author:       Patrick Ohly <patrick.ohly@intel.com>
+# Copyright:    Copyright (C) 2015 Intel Corporation
+#
+# This file is licensed under the MIT license, see COPYING.MIT in
+# this source distribution for the terms.
+
+# A custom scheduler for bitbake which runs rm_work tasks more
+# aggressively than the default schedule chosen by rm_work.
+# To use it, add to local.conf:
+#   BB_SCHEDULERS = "rmwork.RunQueueSchedulerRmWork"
+#   BB_SCHEDULER = "rmwork"
+# Then run:
+#   PYTHONPATH=<path to the directory with rmwork.py> bitbake ...
+
+
+import bb
+from bb.runqueue import RunQueueSchedulerCompletion as BaseScheduler
+
+class RunQueueSchedulerRmWork(BaseScheduler):
+    """
+    Similar to RunQueueSchedulerCompletion, but in addition, rm_work tasks
+    get a priority higher than anything else and get run even when the maximum
+    number of tasks is reached. Together this ensures that nothing blocks running
+    the rm_work tasks once they become ready to run.
+    """
+    name = "rmwork"
+
+    def __init__(self, runqueue, rqdata):
+        BaseScheduler.__init__(self, runqueue, rqdata)
+
+        self.rev_prio_map = range(self.numTasks)
+        for taskid in xrange(self.numTasks):
+            self.rev_prio_map[self.prio_map[taskid]] = taskid
+        bb.note('Original priorities: %s' % [self.describe_task(x) for x in xrange(self.numTasks)])
+
+        self.rmwork_tasks = set()
+        for taskid in xrange(self.numTasks):
+            taskname = self.rqdata.runq_task[taskid]
+            # Detect 'rm_work tasks based on their name.
+            # Kind of a hack, but works...
+            if taskname == 'do_rm_work':
+                self.rmwork_tasks.add(taskid)
+                bb.note('Found do_rm_work: %s' % self.describe_task(taskid))
+
+        # Move all rm_work tasks to the end of the queue (because
+        # later = higher priority).
+        index = 0
+        end = self.numTasks
+        while index < end:
+            taskid = self.prio_map[index]
+            if taskid in self.rmwork_tasks:
+                del self.prio_map[index]
+                self.prio_map.append(taskid)
+                end -= 1
+            else:
+                index += 1
+
+        self.rev_prio_map = range(self.numTasks)
+        for taskid in xrange(self.numTasks):
+            self.rev_prio_map[self.prio_map[taskid]] = taskid
+        bb.note('Modified priorities: %s' % [self.describe_task(x) for x in xrange(self.numTasks)])
+
+    def next(self):
+        taskid = self.next_buildable_task()
+        if taskid is not None:
+            if self.rq.stats.active < self.rq.number_tasks:
+                return taskid
+            if taskid in self.rmwork_tasks:
+                bb.note('Choosing task %s despite task limit.' % self.describe_task(taskid))
+                return taskid
+
+    def describe_task(self, taskid):
+        result = 'ID %d = %s' % (taskid, self.rqdata.get_user_idstring(taskid))
+        if self.rev_prio_map:
+            result = result + (' pri %d' % self.rev_prio_map[taskid])
+        return result
